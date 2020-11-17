26c7358b798f4d9bfbe59ca3dc1cc14efc96728e
jonahswift <18612187212@168.com>
2020-11-17 20:55:51 +0800

    study111

--- firstDay/studyGames.py
+++ firstDay/studyGames.py
@@ -12,5 +12,30 @@ a==1 and b==2
 '''
 import random
 #随机数
+#num = random.randint(1,3)
+#print(num)
+
+puit = str(input('请猜拳：'))
 num = random.randint(1,3)
-print(num)
+'''
+if num == 1:
+    a ='石头'
+   print(f'电脑出的是{a}')
+elif num==2:
+    a = '剪子'
+   # print(f'电脑出的是{a}')
+else:
+    a = '布'
+    #print(f'电脑出的是{a}')
+#print(f'电脑出的是{num}')
+'''
+
+if (puit == '石头' and num == 1)or(puit == '剪子' and num == 2)or(puit == '布' and num == 3):
+    print(f'你出的{puit},电脑出的{num},打平了')
+elif (puit == '石头' and num == 2)or(puit == '剪子' and num == 3)or(puit == '布' and num == 1):
+    print(f'你出的{puit},电脑出的{num},你赢了')
+else:
+    print(f'你出的{puit},电脑出的{num},你输了')
+
+
+
